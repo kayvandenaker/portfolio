5199735b06a0b5ae250480808fa0a0f94bb0d40a
Kay van den Aker <kayakkie87@gmail.com>
2022-01-12 10:08:48 +0100

    bg mobile fix

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import { BrowserRouter} from 'react-router-dom';
 
 ReactDOM.render(
-  <Suspense fallback={<h1>Loading...</h1>}>
+  <Suspense fallback={<h3></h3>}>
     <BrowserRouter>
       <App />
     </BrowserRouter> 
